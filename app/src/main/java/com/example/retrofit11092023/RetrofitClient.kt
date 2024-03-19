3260d1d7e1feafc5998cfd1549ddc98536744c5c
phamtanphat <phatdroid94@gmail.com>
2024-03-19 20:23:24 +0700

    setup RetrofitClient

--- app/src/main/java/com/example/retrofit11092023/RetrofitClient.kt
+++ app/src/main/java/com/example/retrofit11092023/RetrofitClient.kt
@@ -1,17 +1,32 @@
 package com.example.retrofit11092023
 
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     private val retrofit: Retrofit by lazy { createRetrofit() }
+    private val apiService = retrofit.create(ApiService::class.java)
 
-    fun getInstance(): Retrofit {
-        return retrofit
-    }
+    fun getRetrofit() = retrofit
+
+    fun getApiService() = apiService
 
     private fun createRetrofit(): Retrofit {
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+
+        val gson = GsonBuilder().create()
+
         return Retrofit.Builder()
-                .baseUrl("")
-                .build()
+            .baseUrl("http://androidapp.infinityfreeapp.com/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(okHttpClient)
+            .build()
     }
 }
